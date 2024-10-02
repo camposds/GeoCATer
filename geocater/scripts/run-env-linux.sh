ea0555f4dafbafa6b1c33e5e63e710a8e5ade38b
camposds <diegosousa.campos@gmail.com>
2024-10-01 22:27:25 -0300

    environment adjustments

--- geocater/scripts/run-env-linux.sh
+++ geocater/scripts/run-env-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-QGIS_PREFIX_PATH=/usr/
+QGIS_PREFIX_PATH=/usr
 if [ -n "$1" ]; then
     QGIS_PREFIX_PATH=$1
 fi
@@ -10,14 +10,14 @@ echo ${QGIS_PREFIX_PATH}
 
 export QGIS_PREFIX_PATH=${QGIS_PREFIX_PATH}
 export QGIS_PATH=${QGIS_PREFIX_PATH}
-export LD_LIBRARY_PATH=${QGIS_PREFIX_PATH}/lib
-export PYTHONPATH=${QGIS_PREFIX_PATH}/share/qgis/python:${QGIS_PREFIX_PATH}/share/qgis/python/plugins:${PYTHONPATH}
+export LD_LIBRARY_PATH=${QGIS_PREFIX_PATH}/lib/grass82/lib
+export PYTHONPATH=${QGIS_PREFIX_PATH}/share/qgis/python/plugins:${QGIS_PREFIX_PATH}/share/qgis/python:$PYTHONPATH
 
 echo "QGIS PATH: $QGIS_PREFIX_PATH"
 export QGIS_DEBUG=0
 export QGIS_LOG_FILE=/tmp/inasafe/realtime/logs/qgis.log
 
-export PATH=${QGIS_PREFIX_PATH}/bin:$PATH
+export PATH=${QGIS_PREFIX_PATH}/local/bin:${QGIS_PREFIX_PATH}/bin:/bin:${QGIS_PREFIX_PATH}/local/games:${QGIS_PREFIX_PATH}/games:$PATH
 
 echo "This script is intended to be sourced to set up your shell to"
 echo "use a QGIS 3.0 built in $QGIS_PREFIX_PATH"
